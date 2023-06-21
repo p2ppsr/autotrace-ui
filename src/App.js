6365e4eda1423b2bc0b2a07f0f38dc9768841fa2
Gabriel McCormick <74574750+ctrle-mg-resu@users.noreply.github.com>
2023-06-21 16:23:28 -0600

    Formatting updates

--- src/App.js
+++ src/App.js
@@ -12,41 +12,41 @@ import { CssBaseline } from '@mui/material'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#333333'
-        },
-        secondary: {
-            main: '#DB7640'
-        }
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#333333'
+    },
+    secondary: {
+      main: '#DB7640'
     }
+  }
 })
 
 const App = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline>
-                <Router>
-                    <div>
-                        <Header />
-                        <div>
-                            <Routes>
-                                <Route exact path='/' element={<Home />} />
-                                <Route exact path='/Register' element={<Register />} />
-                                <Route exact path='/Trace' element={<Trace />} />
-                                <Route exact path='/Transfer' element={<Transfer />} />
-                                <Route exact path='/Receive' element={<Receive />} />
-                                <Route exacte path='/RepairRequest' element={<RepairRequest />} />
-                                <Route exacte path='/RepairResponse' element={<RepairResponse />} />
-                                <Route path='*' element={<Home />} />
-                            </Routes>
-                        </div>
-                    </div>
-                </Router>
-            </CssBaseline>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Router>
+          <div>
+            <Header />
+            <div>
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/Register' element={<Register />} />
+                <Route exact path='/Trace' element={<Trace />} />
+                <Route exact path='/Transfer' element={<Transfer />} />
+                <Route exact path='/Receive' element={<Receive />} />
+                <Route exacte path='/RepairRequest' element={<RepairRequest />} />
+                <Route exacte path='/RepairResponse' element={<RepairResponse />} />
+                <Route path='*' element={<Home />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </CssBaseline>
+    </ThemeProvider>
+  )
 }
 
 export default App
