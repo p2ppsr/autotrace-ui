3eeda6bd4b8c6724cd0c1643770a0f018de37b74
Ty Everett <ty@tyweb.us>
2023-06-21 16:17:14 -0700

    Fix renderer

--- src/pages/Renderer.jsx
+++ src/pages/Renderer.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 export default ({ content }) => {
-  const parsed = JSON.parse(content)
+  let parsed
+  try {
+    parsed = JSON.parse(content)
+  } catch (e) {
+    console.error(e)
+    return null
+  }
 
   return (
     <div>
       {Object.entries(parsed).map(([k, v]) => (
-        <p style={{ display: 'inline', paddingRight: '2em' }}><b>{k}</b>: {v}</p>
+        <p style={{ display: 'inline', paddingRight: '2em' }}><b>{k}</b>:&nbsp;{v}</p>
       ))}
     </div>
   )
